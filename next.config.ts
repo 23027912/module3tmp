9dbe6397dadf3437478050e6be7c929612c202a7
23027912 <23027912@students.latrobe.edu.au>
2026-07-28 09:38:01 +1000

    add all

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+// next.config.js
+module.exports = {
+  allowedDevOrigins: ['192.168.1.18'],
+}
+export default nextConfig;
